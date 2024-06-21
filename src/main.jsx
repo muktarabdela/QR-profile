49bdc1591c9eaeca2f01f50baa9898dd48a14cc2
muktar <muktarabdela10@gmail.com>
2024-06-21 20:26:31 +0300

    loglib

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Loglib from "@loglib/tracker/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+    <Loglib
+      config={{
+        id: "pharmedia",
+      }}
+    />
+  </>
 )
+
